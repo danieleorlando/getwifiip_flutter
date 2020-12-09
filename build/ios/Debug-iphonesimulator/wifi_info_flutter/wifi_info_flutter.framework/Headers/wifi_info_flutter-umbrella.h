8a68640704f4aeb46e8e26f72dc60e6095488b99
Daniele Orlando <daniele.orlanod@gmail.com>
2020-12-09 23:59:15 +0100

    first commit

---
+++ build/ios/Debug-iphonesimulator/wifi_info_flutter/wifi_info_flutter.framework/Headers/wifi_info_flutter-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "FLTWifiInfoLocationHandler.h"
+#import "WifiInfoFlutterPlugin.h"
+
+FOUNDATION_EXPORT double wifi_info_flutterVersionNumber;
+FOUNDATION_EXPORT const unsigned char wifi_info_flutterVersionString[];
+
